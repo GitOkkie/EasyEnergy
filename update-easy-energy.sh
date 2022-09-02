e14a1edbf643cb7d4f5600f8157a68a7f9d4aceb
Oscar <git@okkie.nu>
2022-09-02 11:56:02 +0200

    add script for crontab

---
+++ update-easy-energy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# run dit vanuit je favoriete cron-variant, bijvoorbeeld:
+# 10 * * * * /usr/local/bin/update-easy-energy.sh
+
+$DOCROOT=/var/www/html/
+$EASYAPI=/usr/local/bin/easyapi.py
+
+cd $DOCROOT || exit 1
+
+$EASYAPI > ee.new 
+
+diff -u ee.txt ee.new | grep ^+[^+]
+mv -f ee.new ee.txt
