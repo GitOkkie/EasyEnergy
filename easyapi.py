283a1a736e5a318608a2ec8cdf22705a4e4c6b0a
Oscar <git@okkie.nu>
2022-11-27 19:18:31 +0100

    change of hard coded "taxes" per 1 dec 2022

--- easyapi.py
+++ easyapi.py
@@ -11,6 +11,9 @@ utc_offset=None
 tax_stroom_ct = 0.872 + 4.010 + 3.325 + 0.104
 tax_gas_ct = 4.23 + 1.714 + 39.591 + 9.429
 
+# na 1 dec:
+# tax_stroom_ct = 1.962 + 4.010 + 3.325 + 0.104
+# tax_gas_ct = 9.81 + 1.714 + 39.591 + 9.429
 
 def get_easy_data(url, ts1, ts2):
     global verify_ssl_cert
